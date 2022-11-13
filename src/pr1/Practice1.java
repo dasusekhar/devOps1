d66c6af769c899d0e94376dd0d04094bfc5f869a
dasusekhar4130@gmail.com <dasusekhar4130@gmail.com>
2022-11-13 21:46:27 +0530

    second commit message

--- src/pr1/Practice1.java
+++ src/pr1/Practice1.java
@@ -3,5 +3,6 @@ package pr1;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("welcome to devops learing journey");
+        System.out.println("started git course");
     }
 }
