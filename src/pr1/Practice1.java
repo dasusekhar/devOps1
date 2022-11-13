264863955b7e7a8ea715f28e2f7b43a8bb7c531b
dasusekhar4130@gmail.com <dasusekhar4130@gmail.com>
2022-11-13 21:49:50 +0530

    dev1 first comment

--- src/pr1/Practice1.java
+++ src/pr1/Practice1.java
@@ -4,5 +4,6 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("welcome to devops learing journey");
         System.out.println("started git course");
+        System.out.println("dev1 is commented");
     }
 }
